f3a4c7b6149719e07cee9d99f2ec0ebc8bd191b8
Fellipe Borges <fellipe.borges@br.imshealth.com>
2020-06-29 17:37:46 -0300

    logic to show random question

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -18,12 +18,46 @@ export class QuestionComponent implements OnInit {
     this.getQuestions();
   }
 
+  // Retrieves the questions from Questions Service and shows the next random question.
   getQuestions(): void {
     this.questionService.getQuestions()
-      .subscribe(q => this.questions = q);
+      .subscribe(q => {
+        this.questions = [...q];
+        this.showNextRandomQuestion();
+      });
   }
 
-  onNewQuestion(): void {
-    this.current = this.questions[2];
+  // Selects a random question from the array of questions and set it to be shown in the view to the user.
+  // After selecting a question it's removed from the array.
+  // If the questions array is empty nothing is done.
+  showNextRandomQuestion(): void {
+    // Cleans the current question
+    this.current = undefined;
+
+    // Checks if the array still contains questions
+    if (!this.questions){
+      return;
+    }
+
+    // Generates a random number between array's first and last position
+    const min = 0;
+    const max: number = this.questions.length - 1;
+    const index: number = Math.floor(Math.random() * (max - min + 1) + min);
+
+    // Sets the current questions
+    this.current = this.questions[index];
+
+    // Removes the question from the array
+    this.questions.splice(index, 1);
+  }
+
+  // Checks if there are more questions to be shown
+  noFurtherQuestions(): boolean {
+    return this.questions.length === 0 && !this.current;
+  }
+
+  // Rebuilds the list of questions
+  restartQuestions(): void {
+    this.getQuestions();
   }
 }
