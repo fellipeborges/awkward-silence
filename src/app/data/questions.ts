cb788faaca519ecf182293ace63f2d8301546a7b
Fellipe Borges <fellipe.borges@br.imshealth.com>
2020-06-30 18:33:10 -0300

    question UI is done

--- src/app/data/questions.ts
+++ src/app/data/questions.ts
@@ -1,14 +1,10 @@
 import { Question } from '../model/question';
 
 export const QUESTIONS: Question[] = [
-  {text: 'Question 0'},
-  {text: 'Question 1'},
-  {text: 'Question 2'},
-  {text: 'Question 3'},
-  {text: 'Question 4'},
-  // {id: 5, text: 'Question 5'},
-  // {id: 6, text: 'Question 6'},
-  // {id: 7, text: 'Question 7'},
-  // {id: 8, text: 'Question 8'},
-  // {id: 9, text: 'Question 9'}
+  {text: 'Bill Gates said, "I will always choose a lazy person to do a difficult job because a lazy person will find an easy way to do it." What\'s a real-life example of this?'},
+  {text: 'What are some VERY comforting facts?'},
+  {text: 'What are some VERY creepy facts?'},
+  {text: 'What was the stupidest thing you believed as a kid?'},
+  {text: 'If your life would be a perfume, what would be its name and how would it smell?'},
+  {text: 'How do people in your life say "I love you" without saying "I love you?"'},
 ];
