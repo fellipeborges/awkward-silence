63bf4923faadb9c495cc8fa520ce53ac701c1095
Fellipe Borges <fellipe.borges@br.imshealth.com>
2020-07-09 17:51:19 -0300

    Added new questions. Now we have 100.

--- src/app/data/questions.ts
+++ src/app/data/questions.ts
@@ -72,27 +72,339 @@ export const QUESTIONS: Question[] = [
     ptBr: 'Você está controlando o Twitter da JK Rowling por um dia. Que informação desnecessária você adiciona ao mundo de Harry Potter?'
   },
   {
-    enUs: '',
-    ptBr: ''
+    enUs: 'When you are close to a balcony, do you feel a strange desire to jump?',
+    ptBr: 'Quando você está perto de uma sacada, você sente uma estranha vontade de se jogar?'
   },
   {
-    enUs: '',
-    ptBr: ''
+    enUs: 'You\'re given the option to drop what you have and return 10 years as yourself at that age, but with the extra 10 years of experience. Do you take it?',
+    ptBr: 'Você tem a opção de largar tudo que tem e voltar exatos 10 anos como você mesmo naquela idade mas com 10 anos de experiência extra. Você voltaria?'
   },
   {
-    enUs: '',
-    ptBr: ''
+    enUs: 'What looks easy peasy lemon squeezy but is actually difficult difficult lemon difficult?',
+    ptBr: 'O que parece muito muito muito fácil mas na verdade é muito muito muito difícil?'
   },
   {
-    enUs: '',
-    ptBr: ''
+    enUs: 'What do you think is the single greatest photograph in history?',
+    ptBr: 'Qual você acha que é a melhor fotografia de todos os tempos?'
   },
   {
-    enUs: '',
-    ptBr: ''
+    enUs: 'The words \'Low Battery\' appear right before your eyes, you take off your VR headset and realized your whole life was just a VR simulation which you\'ve been playing for only a few minutes. How do you react?',
+    ptBr: 'As palavras \'Bateria fraca\' aparecem bem na frente de seus olhos, você tira seu óculos de realidade virtual e percebe que sua vida inteira foi uma simulação que você estava jogando por alguns minutos. Como você reagiria?'
   },
   {
-    enUs: '',
-    ptBr: ''
+    enUs: 'People who are 40+ and happy with their life, what is your advice to people in their 20s?',
+    ptBr: 'Pessoas com 40 anos ou mais e que são felizes com o rumo de suas vidas, que conselhos daria para pessoas na faixa de 20 anos?'
   },
+  {
+    enUs: 'Which job is a LOT less fun than most people expect?',
+    ptBr: 'Que trabalho é BEM menos divertido do que a maioria das pessoas acha?'
+  },
+  {
+    enUs: 'What song is 10/10, yet hardly anyone has heard of it?',
+    ptBr: 'Qual é a música nota 10 que quase ninguém ouviu ainda?'
+  },
+  {
+    enUs: 'Have you ever woke up in the middle of a dream and the dream was so interesting that you wanted to go back to sleep to see the end of it? If so, what was the dream?',
+    ptBr: 'Você já acordou no meio de um sonho tão interessante que você queria voltar a dormir para poder ver como ele terminaria? Se sim, que sonho era esse?'
+  },
+  {
+    enUs: 'Without saying what the category is, what are your top five?',
+    ptBr: 'Sem dizer qual a categoria, elenque seu TOP 5 de algo.'
+  },
+  {
+    enUs: 'What’s something small you can start doing today to better yourself?',
+    ptBr: 'Qual a coisa pequena que você pode começar a fazer hoje mesmo que vai te deixar melhor de alguma forma?'
+  },
+  {
+    enUs: 'What is the coolest website you’ve visited that no one knows about?',
+    ptBr: 'Qual é o site mais legal que você conhece mas que ninguém conhece?'
+  },
+  {
+    enUs: 'What mind-blowing (but simple) facts would satisfy the curiosity a 4-year old child before bedtime?',
+    ptBr: 'Quais são os fatos de explodir cabeça (mas simples) que iriam satisfazer a curiosidade uma criança de 4 anos antes de dormir?'
+  },
+  {
+    enUs: 'In a video game, if you come across an empty room with a health pack, extra ammo, and a save point, you know some serious shit is about to go down. What is the real-life equivalent of this?',
+    ptBr: 'Num vídeo game, se você passa por uma sala com suplemento de vida, munições e um save point, você sabe que vem chumbo grosso pela frente. Qual é o equivalente disso na vida real?'
+  },
+  {
+    enUs: 'There’s a population of 7.5 billion humans and 19 billion chickens at any given time. If there was a chicken rebellion how would you prepare to fight off your 2.7 chickens?',
+    ptBr: 'Existe uma população de 7.5 bilhões de humanos e 19 bilhões de galinhas. Se houvesse uma rebelião das aves como você se prepararia para lutar contra suas 2.7 galinhas?'
+  },
+  {
+    enUs: 'What do you use to remind yourself that everything isn\'t that bad?',
+    ptBr: 'O que você usa para lembrar a si próprio que nem tudo é tão ruim?'
+  },
+  {
+    enUs: 'What harmful things are being taught to children?',
+    ptBr: 'Que coisas que fazem mal estão sendo ensinadas às crianças?'
+  },
+  {
+    enUs: 'If scientists invented a teleportation system but the death rate was 1 in 5 million would you use it?',
+    ptBr: 'Se cientistas inventassem um sistema de teletransporte mas com uma taxa de morte de 1 em 5 milhões, você usaria?'
+  },
+  {
+    enUs: 'What is the best TV show ever?',
+    ptBr: 'Qual a melhor série de TV de todos os tempos?'
+  },
+  {
+    enUs: 'What is the best burger place you have been to?',
+    ptBr: 'Qual é a melhor hamburgeria que você já foi?'
+  },
+  {
+    enUs: 'You get a billion dollars if you can start a massive argument at your family\'s Thanksgiving dinner. What\'s your game plan?',
+    ptBr: 'Você receberá 1 bilhão de reais caso consiga iniciar uma discussão gigantesca no jantar de Natal da sua família. Qual seria seu plano?'
+  },
+  {
+    enUs: 'What do you want for Christmas?',
+    ptBr: 'O que você quer de Natal?'
+  },
+  {
+    enUs: 'What a good TV series that can pass the time in, let’s say.... 14 days?',
+    ptBr: 'Qual uma série de TV boa para passar o tempo em... 14 dias?'
+  },
+  {
+    enUs: 'If you had died one year ago, what beautiful things would you have had missed out on?',
+    ptBr: 'Se você tivesse morrido há 1 ano, que coisas boas você teria perdido?'
+  },
+  {
+    enUs: 'How do you feel about a 4 day work week?',
+    ptBr: 'O que você acha de termos apenas 4 dias úteis na semana?'
+  },
+  {
+    enUs: 'You are offered $1,000,000 USD if you can hide a pair of car keys from the entirety of the FBI force for 7 days. Where do you hide the keys?',
+    ptBr: 'Lhe é oferecido 1 milhão de dólares se você conseguir esconder de todo o FBI as chaves do carro por 7 dias. Onde você as esconderia?'
+  },
+  {
+    enUs: 'What’s the most amazing thing about the universe?',
+    ptBr: 'Qual é a coisa mais incrível sobre universo?'
+  },
+  {
+    enUs: 'Which conspiracy theory is so believable that it might be true?',
+    ptBr: 'Que teoria da conspiração é tão crível que pode até ser verdade?'
+  },
+  {
+    enUs: 'What free software is so good you can\'t believe it\'s available for free?',
+    ptBr: 'Qual software gratuito é tão bom que nem dá pra acreditar que não custa nada?'
+  },
+  {
+    enUs: 'If you like to listen to full albums, what are your favorite albums to binge?',
+    ptBr: 'Se você gosta de escutar álguns inteiros, quais são seus favoritos para fazer isso?'
+  },
+  {
+    enUs: 'What’s the cringiest thing you’ve seen a bride and groom do for their wedding?',
+    ptBr: 'Qual foi a coisa mais vergonha alheia que você já viu um noivo/noiva fazendo em um casamento?'
+  },
+  {
+    enUs: 'What is a good weakness to mention on a job interview?',
+    ptBr: 'Qual é um bom ponto fraco para mencionar em uma entrevista de trabalho?'
+  },
+  {
+    enUs: 'What the hell are you supposed to do when having happy birthday sung to you?',
+    ptBr: 'Que diabos você tem que fazer quando Parabéns pra Você está sendo cantando pra você?'
+  },
+  {
+    enUs: 'What golden nugget of information do you have to share?',
+    ptBr: 'Que informação valiosíssima você tem para compartilhar?'
+  },
+  {
+    enUs: 'Rather than eating a family size bag of Doritos in 20 minutes, what are some healthy snack alternatives that can be consumed in large amounts?',
+    ptBr: 'Ao invés de comer um pacote de Doritos tamanho família em 20 minutos, qual comida saudável é uma alternativa para ser consumida em grandes quantidades?'
+  },
+  {
+    enUs: 'You are teleported into a game of your choice for a month. If you die in the game you die in real life BUT whatever you collect in the game you can keep in real life. What game do you pick?',
+    ptBr: 'Você é teletransportado para um jogo de sua escolhar por um mês. Se você morrer no jogo também morre na vida real MAS qualquer coisa que você coletar no jogo fica pra você na vida real. Que jogo você escolhe?'
+  },
+  {
+    enUs: 'What is something about yourself that sounds totally made up but is 100% real?',
+    ptBr: 'Que fato sobre você parece muito inventado mas é 100% real?'
+  },
+  {
+    enUs: 'Confucius once said “We all have two lives, the second one starts when you realise you only have one”. When and why did your second life begin?',
+    ptBr: 'Confúcio disse uma vez "Todos nós temos duas vidas, a segunda começa quando você se dá conta que tem apenas uma". Quando sua segunda vida começou?'
+  },
+  {
+    enUs: 'What did a fictional character say that stuck with you?',
+    ptBr: 'O que um personagem de ficção disse que ficou na sua cabeça?'
+  },
+  {
+    enUs: 'What screams "I\'m very insecure"?',
+    ptBr: 'O que deixa muito claro que a pessoa é insegura?'
+  },
+  {
+    enUs: 'What\'s a good website to waste a whole night on?',
+    ptBr: 'Qual é um bom site para se gastar a noite toda?'
+  },
+  {
+    enUs: 'What do people learn too late?',
+    ptBr: 'O que as pessoas aprendem quando já é muito tarde?'
+  },
+  {
+    enUs: 'You were offered immortality, do you accept it?',
+    ptBr: 'Lhe é oferecida a imortalidade. Você aceita?'
+  },
+  {
+    enUs: 'Besides eating cereal with water what is the most outrageous "eating sin" you have ever witnessed?',
+    ptBr: 'Além de comer cereal com água, qual foi o maior pecado que você já viu alguém fazendo com comida?'
+  },
+  {
+    enUs: 'You get $1,000,000 however, it\'s because $100 is taken from 10,000 random people. If nobody knows that you got the money, would you do it?',
+    ptBr: 'Você ganha 1 milhão de reais mas 100 reais são retirados de 10.000 pessoas aleatórias. Se ninguém soubesse que você pegou o dinheiro, você faria isso?'
+  },
+  {
+    enUs: 'What was a huge trend that everyone forgot about?',
+    ptBr: 'O que era uma mega tendência/moda que todo mundo já esqueceu?'
+  },
+  {
+    enUs: 'What do you think is scarier, the idea that we are alone in the universe or the idea that we aren’t? Why?',
+    ptBr: 'O que você acha mais amedrontador, o fato de estarmos sozinhos no universo ou de não estarmos sozinhos? Por que?'
+  },
+  {
+    enUs: 'What YouTube channels are genuinely worth watching?',
+    ptBr: 'Que canais do YouTube genuinamente valem a pena de assistir?'
+  },
+  {
+    enUs: 'How do you tell if someone is attracted to you or just being very friendly?',
+    ptBr: 'Como você diferencia se a pessoa está amorosamente atraída por você ou está apenas sendo educada?'
+  },
+  {
+    enUs: 'What\'s a joke that\'s so stupid it\'s funny?',
+    ptBr: 'Qual a piada que é tão estúpida que chega a ser engraçada?'
+  },
+  {
+    enUs: 'What is something most people need to hear but no one has the guts to tell people?',
+    ptBr: 'O que a maioria das pessoas precisa ouvir mas ninguém tem coragem de dizer a elas?'
+  },
+  {
+    enUs: 'As of right now, what will it take to make you 100% happy?',
+    ptBr: 'O que precisaria para você ficar 100% feliz agora?'
+  },
+  {
+    enUs: 'What is an adult problem you were not prepared for?',
+    ptBr: 'Qual é um problema de adulto que você não estava preparado para enfrentar?'
+  },
+  {
+    enUs: 'What did your crush do that absolutely killed your interest?',
+    ptBr: 'O que o seu crush fez que te fez perder todo seu interesse nele(a)?'
+  },
+  {
+    enUs: 'What are some common "facts" that people believe to be true even if the existing evidence states the contrary?',
+    ptBr: 'Quais são as crenças do senso comum que todo mundo acredita serem verdadeiras mesmo com evidências dizendo o contrário?'
+  },
+  {
+    enUs: 'What things did you do as a kid that you now realize is extremely weird?',
+    ptBr: 'Que coisas que você fazia quando era criança que agora parecem extremamente estranhas?'
+  },
+  {
+    enUs: 'What’s something that people think makes them look cool but actually has the opposite effect?',
+    ptBr: 'O que as pessoas acham que as fazem parecer descoladas mas na verdade tem o efeito oposto?'
+  },
+  {
+    enUs: 'You are sent back to the day the Titanic set sail with nothing but a 3rd class ticket. Your task is to stop the titanic from sinking, how do you do this?',
+    ptBr: 'Você é enviado no passado para o dia que o Titanic partiu com apenas uma passagem para a 3a classe. Sua tarefa é evitar que o navio afunde. Como você faz isso?'
+  },
+  {
+    enUs: 'Would you reduce your meat consumption if lab-grown meat or meat alternatives were cheaper and tasted good?',
+    ptBr: 'Você reduziria seu consumo de carne se carne feita em laboratório ou outras alternativas fossem mais baratas e tivessem um excelente sabor?'
+  },
+  {
+    enUs: 'What was a house rule you had as a kid that you thought was completely normal until you grew up and realized not all households followed?',
+    ptBr: 'Que regras haviam na sua casa quando você era criança e você achava normal e depois de crescer percebeu que nem todas as casas as seguiam?'
+  },
+  {
+    enUs: 'If someone borrowed your body for a week, what quirks would you tell them about so they are prepared?',
+    ptBr: 'Se alguém pegasse seu corpo emprestado por uma semana, sobre qual peculiaridades você deixaria a pessoa ciente para estar preparada?'
+  },
+  {
+    enUs: 'What is most useless item you carry with you all the time?',
+    ptBr: 'Qual é o item mais inútil que você carrega consigo o tempo todo?'
+  },
+  {
+    enUs: 'What is something that has aged well?',
+    ptBr: 'O que envelheceu bem?'
+  },
+  {
+    enUs: 'What happened today that made you happy?',
+    ptBr: 'O que aconteceu hoje que te fez feliz?'
+  },
+  {
+    enUs: 'How would you feel about a ‘None of the above’ option in elections. If that option wins, the election will be held again with new candidates?',
+    ptBr: 'O que você acha se nas eleições tivesse uma opção "Nenhum dos anteriores" e, caso essaopção vencesse, uma nova eleição seria feita com outros candidatos?'
+  },
+  {
+    enUs: 'What small thing makes you automatically distrust someone?',
+    ptBr: 'Que pequenas coisas faz você automaticamente não confiar em alguém?'
+  },
+  {
+    enUs: 'If someone says “I love you” but you’re not ready to say it back, how do you respond?',
+    ptBr: 'Se alguém diz "Eu te amo" mas você não está pronto para dizer também, como você responde?'
+  },
+  {
+    enUs: 'How do you feel about a law stating that tickets (speeding, driving under influence etc.) should be a percentage of the perpetrators annual wage, instead of fixed rates?',
+    ptBr: 'O que você acha de uma lei que fizesse com que as multas de trânsito fossem um percentual da renda do infrator ao invés de um valor fixo?'
+  },
+  {
+    enUs: 'What celebrity did bad things but everyone "forgot" what they did because they\'re famous?',
+    ptBr: 'Que celebridade fez coisas horríveis mas todos "esqueceram" porque ele(a) é famoso(a)?'
+  },
+  {
+    enUs: 'What’s a sign that somebody wasn’t raised right?',
+    ptBr: 'Qual é um indicador que alguém não foi criado direito?'
+  },
+  {
+    enUs: 'What is normal in your country but seems weird to the rest of the world?',
+    ptBr: 'O que é normal no seu país mas parece estranho para o resto do mundo?'
+  },
+  {
+    enUs: 'How do you control yourself when you are full of rage and want to keep calm?',
+    ptBr: 'Como você se controla quando está com muita raiva e quer ficar calmo?'
+  },
+  {
+    enUs: 'What’s something that is totally normal in movies, but never happens in real life?',
+    ptBr: 'O que é totalmente normal em filmes mas nunca acontece na vida real?'
+  },
+  {
+    enUs: 'What\'s one of the dumbest things you\'ve ever spent money on?',
+    ptBr: 'Qual é a coisa mais imbecil com que você gastou dinheiro?'
+  },
+  {
+    enUs: 'You are transported to year 0, with any object from the present with you. Which item do you choose to make people lose their minds the most?',
+    ptBr: 'Você é tele transportado para o ano 0 com qualquer objeto do presente com você. Que objeto você escolheria levar que deixaria as pessoas completamente loucas?'
+  },
+  {
+    enUs: 'What is one of the dumbest things you\'ve done when your brain was on "auto-pilot"?',
+    ptBr: 'Qual a coisa mais estúpida você já fez quando seu cérebro estava no modo de "piloto-automático"?'
+  },
+  {
+    enUs: 'What are some good first date questions to get to know someone?',
+    ptBr: 'Quais são boas perguntas para se fazer em um primeiro encontro para conhecer melhor a outra pessoa?'
+  },
+  {
+    enUs: 'What film role was 100% perfectly cast?',
+    ptBr: 'Que papel de filme teve um ator/atri escolhido com 100% de perfeição?'
+  },
+  {
+    enUs: 'How does someone politely end a conversation with a person who won\'t stop talking?',
+    ptBr: 'Como se termina educadamente uma conversa com uma pessoa que não para de falar?'
+  },
+  {
+    enUs: 'Would you rather have hiccups for the rest of your life or constantly feel like you have to sneeze?',
+    ptBr: 'Você preferiria ter soluços pros resto da vida ou ficar constantemente com a sensação de que vai espirrar?'
+  },
+  {
+    enUs: 'Would you rather be ugly but smell amazing or be handsome/beautiful but stink?',
+    ptBr: 'Você preferiria ser feio mas cheirar maravilhosamente bem ou ser bonito(a) e cheirar mal?'
+  },
+  {
+    enUs: 'Would you rather speak every language perfectly or play every instrument perfectly?',
+    ptBr: 'Você preferiria falar todas as línguas perfeitamente ou tocar todos os instrumentos perfeitamente?'
+  },
+  {
+    enUs: 'What’s a suitable punishment for people who litter in national parks?',
+    ptBr: 'Qual seria uma punição justa para pessoas que jogam lixo no chão em parques?'
+  },
+  {
+    enUs: 'What movie did you start watching then said "To the hell with this, I\'m not finishing this"?',
+    ptBr: 'Que filme você começou a assistir e disse "Pro inferno com isso, eu não vou terminar de jeito nenhum"?'
+  }
 ];
